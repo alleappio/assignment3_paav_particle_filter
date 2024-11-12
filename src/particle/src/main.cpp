47c07808bdacf149d234e7ed7f6deb3797485a8c
alleappio <allevanny@gmail.com>
2024-11-13 00:19:16 +0100

    adjusted parameters

--- src/particle/src/main.cpp
+++ src/particle/src/main.cpp
@@ -35,8 +35,8 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_particles(new pcl::PointCloud<pcl::Poi
 * Define the proper noise values
 */
 double sigma_init [3] = {0.01, 0.01, 0.01};  //[x,y,theta] initialization noise. 
-double sigma_pos [3]  = {0.15, 0.15, 0.15}; //[x,y,theta] movement noise. Try values between [0.5 and 0.01]
-double sigma_landmark [2] = {0.20, 0.20};     //[x,y] sensor measurement noise. Try values between [0.5 and 0.1]
+double sigma_pos [3]  = {0.17, 0.17, 0.17}; //[x,y,theta] movement noise. Try values between [0.5 and 0.01]
+double sigma_landmark [2] = {0.21, 0.21};     //[x,y] sensor measurement noise. Try values between [0.5 and 0.1]
 std::vector<Color> colors = {Color(1,0,0), Color(1,1,0), Color(0,0,1), Color(1,0,1), Color(0,1,1)};
 control_s odom;
 
